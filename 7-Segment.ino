7b3d75e799e1ea5ca7bd8b88927fee6fce4d49b9
Ismaturrofiah <73883487+Ismaturrofiah@users.noreply.github.com>
2022-01-16 12:11:13 +0700

    Add files via upload

---
+++ 7-Segment.ino
@@ -0,0 +1,310 @@
+void setup() {
+  // put your setup code here, to run once:
+  for(int i=22;i<38;i++){
+    pinMode(i,OUTPUT);
+    }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//ANGKA 8
+digitalWrite(22,0);
+digitalWrite(23,0);
+digitalWrite(24,1);
+digitalWrite(25,1);
+digitalWrite(26,0);
+digitalWrite(27,0);
+digitalWrite(28,1);
+digitalWrite(29,0);
+digitalWrite(30,0);
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,1);
+digitalWrite(36,1);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 7
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,0);
+digitalWrite(35,0);
+digitalWrite(36,0);
+digitalWrite(37,0);
+delay(500);
+//ANGKA 6
+digitalWrite(31,1);
+digitalWrite(32,0);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,1);
+digitalWrite(36,1);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 5
+digitalWrite(31,1);
+digitalWrite(32,0);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,0);
+digitalWrite(36,1);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 4
+digitalWrite(31,0);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,0);
+digitalWrite(35,0);
+digitalWrite(36,1);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 3
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,0);
+digitalWrite(36,0);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 2
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,0);
+digitalWrite(34,1);
+digitalWrite(35,1);
+digitalWrite(36,0);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 1
+digitalWrite(31,0);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,0);
+digitalWrite(35,0);
+digitalWrite(36,0);
+digitalWrite(37,0);
+delay(500);
+//ANGKA 0
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,1);
+digitalWrite(36,1);
+digitalWrite(37,0);
+delay(500);
+digitalWrite(22,0);
+digitalWrite(23,1);
+digitalWrite(24,0);
+digitalWrite(25,0);
+digitalWrite(26,1);
+digitalWrite(27,0);
+digitalWrite(28,0);
+digitalWrite(29,1);
+digitalWrite(30,0);
+delay(500);
+//ANGKA 8
+digitalWrite(22,1);
+digitalWrite(23,0);
+digitalWrite(24,0);
+digitalWrite(25,0);
+digitalWrite(26,0);
+digitalWrite(27,1);
+digitalWrite(28,1);
+digitalWrite(29,0);
+digitalWrite(30,0);
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,1);
+digitalWrite(36,1);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 7
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,0);
+digitalWrite(35,0);
+digitalWrite(36,0);
+digitalWrite(37,0);
+delay(500);
+//ANGKA 6
+digitalWrite(31,1);
+digitalWrite(32,0);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,1);
+digitalWrite(36,1);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 5
+digitalWrite(31,1);
+digitalWrite(32,0);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,0);
+digitalWrite(36,1);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 4
+digitalWrite(31,0);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,0);
+digitalWrite(35,0);
+digitalWrite(36,1);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 3
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,0);
+digitalWrite(36,0);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 2
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,0);
+digitalWrite(34,1);
+digitalWrite(35,1);
+digitalWrite(36,0);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 1
+digitalWrite(31,0);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,0);
+digitalWrite(35,0);
+digitalWrite(36,0);
+digitalWrite(37,0);
+delay(500);
+//ANGKA 0
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,1);
+digitalWrite(36,1);
+digitalWrite(37,0);
+delay(500);
+digitalWrite(22,0);
+digitalWrite(23,1);
+digitalWrite(24,0);
+digitalWrite(25,0);
+digitalWrite(26,1);
+digitalWrite(27,0);
+digitalWrite(28,0);
+digitalWrite(29,1);
+digitalWrite(30,0);
+delay(500);
+//ANGKA 8
+digitalWrite(22,1);
+digitalWrite(23,0);
+digitalWrite(24,0);
+digitalWrite(25,1);
+digitalWrite(26,0);
+digitalWrite(27,0);
+digitalWrite(28,0);
+digitalWrite(29,0);
+digitalWrite(30,1);
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,1);
+digitalWrite(36,1);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 7
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,0);
+digitalWrite(35,0);
+digitalWrite(36,0);
+digitalWrite(37,0);
+delay(500);
+//ANGKA 6
+digitalWrite(31,1);
+digitalWrite(32,0);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,1);
+digitalWrite(36,1);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 5
+digitalWrite(31,1);
+digitalWrite(32,0);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,0);
+digitalWrite(36,1);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 4
+digitalWrite(31,0);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,0);
+digitalWrite(35,0);
+digitalWrite(36,1);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 3
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,0);
+digitalWrite(36,0);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 2
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,0);
+digitalWrite(34,1);
+digitalWrite(35,1);
+digitalWrite(36,0);
+digitalWrite(37,1);
+delay(500);
+//ANGKA 1
+digitalWrite(31,0);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,0);
+digitalWrite(35,0);
+digitalWrite(36,0);
+digitalWrite(37,0);
+delay(500);
+//ANGKA 0
+digitalWrite(31,1);
+digitalWrite(32,1);
+digitalWrite(33,1);
+digitalWrite(34,1);
+digitalWrite(35,1);
+digitalWrite(36,1);
+digitalWrite(37,0);
+delay(500);
+digitalWrite(22,0);
+digitalWrite(23,1);
+digitalWrite(24,0);
+digitalWrite(25,0);
+digitalWrite(26,1);
+digitalWrite(27,0);
+digitalWrite(28,0);
+digitalWrite(29,1);
+digitalWrite(30,0);
+delay(500);
+}
