9b4edbc2ec07c11f74b67b3e503d9ba203b7885c
Ismaturrofiah <73883487+Ismaturrofiah@users.noreply.github.com>
2022-01-16 12:27:35 +0700

    Create Latihan_1.ino

---
+++ LDR/Latihan_1.ino
@@ -0,0 +1,20 @@
+#define PINLDR 1
+#define LED 11
+int sensorLDR = 0;
+int output = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+sensorLDR = analogRead(PINLDR);
+output = map(sensorLDR, 0, 1023, 0, 20);
+if(output <= 2){
+  digitalWrite (LED,HIGH);}
+else{
+  digitalWrite(LED,LOW);}
+delay(500);
+}
