1486e622f73749ff60c1a0142d2da62078b54209
Ismaturrofiah <73883487+Ismaturrofiah@users.noreply.github.com>
2022-01-16 12:23:42 +0700

    Add files via upload

---
+++ LCD/LCD_Latihan_2.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println("A");
+Serial.println("1");
+Serial.println("2");
+Serial.println("3");
+Serial.println("B");
+Serial.println("4");
+Serial.println("5");
+Serial.println("6");
+Serial.end();
+}
